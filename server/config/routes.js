05302a0012e6b47fba15a21d8ec098688ca58945
chengbo_x <chengbo_x@163.com>
2019-03-17 18:35:11 +0800

    online_todolist

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,10 +3,11 @@
 const TodoController = require('../app/controllers/todoController');
 
 module.exports = function(app) {
-    app.get('/api/all', TodoController.getAll);
-    app.post('/api/add', TodoController.newTodo);
-    app.post('/api/deletes', TodoController.deleteOne);
-    app.post('/api/deletemany', TodoController.deleteAllCompleted);
-    app.post('/api/update', TodoController.updateOne);
-    app.post('/api/updateMany', TodoController.updateMany);
+    // todolist 接口
+    app.get('/todolist/api/all', TodoController.getAll);
+    app.post('/todolist/api/add', TodoController.newTodo);
+    app.post('/todolist/api/deletes', TodoController.deleteOne);
+    app.post('/todolist/api/deletemany', TodoController.deleteAllCompleted);
+    app.post('/todolist/api/update', TodoController.updateOne);
+    app.post('/todolist/api/updateMany', TodoController.updateMany);
 }
